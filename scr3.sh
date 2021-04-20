596b3514acdc321274f4f483a82c916fac8ea4d9
aragha <67502036+aragha@users.noreply.github.com>
2021-04-20 17:10:59 -0400

    Create scr3.sh

---
+++ scr3.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+ln -sf	/usr/share/zoneinfo/US/Eastern	/etc/localtime
+echo "en_US.UTF-8" >> /etc/locale.gen
+hwclock	--systohc
+locale-gen
+echo "keymap=US" >> locale-gen
+
+#create hostname file
+echo "archvbox"	/etc/hostname
+
+#local hostname resolution
+echo "127.0.0.1	localhost
+::1	localhost
+127.0.1.1	archvbox.localdomain	archvbox" >> /etc/hosts
+
+#install grub 
+pacman	-S grub
+grub-install	--target=x86_64-efi	bootloader-id=grub-uefi	--recheck
+#make grub config file
+grub-mkconfig	-o	/boot.grub/grub.cfg
+
+#install other necessary tools
+#pacman	-S networkmanager vim
+#enable networkmanager
+systemctl enable 	NetworkManager
+exit
