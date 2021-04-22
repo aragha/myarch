51c80c7f515a4a52ceebea5f1e649af48a24b667
chrooter <aras2k@yahoo.com>
2021-04-22 09:43:47 -0400

    ereplaced locale,gen command updation line with sed line instaed of echo method

--- scr3.sh
+++ scr3.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 ln -sf	/usr/share/zoneinfo/US/Eastern	/etc/localtime
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+#echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+sed -i "s/#en_US.UTF-8/en_US.UTF-8/"
 locale-gen
 hwclock	--systohc
 echo "keymap=US" >> locale-gen
